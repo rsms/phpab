3e426f466aefc6b0f8c5dd402b92656ec3d10344
rasmus <rasmus@9b4f6404-e40b-0410-ad9e-9008e25ae5f1>
2006-07-01 22:21:45 +0000

    Added lint.php
    git-svn-id: http://svn.hunch.se/ab/trunk@91 9b4f6404-e40b-0410-ad9e-9008e25ae5f1

---
+++ tools/lint.php
@@ -0,0 +1,44 @@
+<?
+require_once '../lib/base/boot.php';
+
+
+$php = Process::resolveBinary('php');
+$lib = new File('../lib');
+$allOK = true;
+$scrollTo = '<script type="text/javascript">window.scrollTo(0,100000);</script>';
+
+foreach($lib->getFiles(true) as $file)
+{
+	$path = $file->getAbsolutePath();
+	
+	if(strrchr($path, '.') == '.php')
+	{
+		$cmd = "$php -l '$path' 2>&1";
+		#print "$cmd<br />";
+		$out = `$cmd`;
+		$shortPath = basename(dirname($path)).'/'.basename($path);
+		
+		if(strpos($out, 'No syntax errors') !== false) {
+			print '<span style="color:#8c8">'.$shortPath.'</span><br />';
+		}
+		else {
+			print '<span style="color:#8c8">'.$shortPath.'</span><br />'
+				. '<pre style="border:1px solid #ddd;padding:7px;margin-bottom:10px;">'
+				. htmlentities($out)
+				. '</pre>';
+			
+			$allOK = false;
+		}
+		
+		print $scrollTo;
+		
+		flush();
+	}
+}
+
+if($allOK)
+	print '<hr /><h2>All okay!</h2>'.$scrollTo;
+else
+	print '<hr /><h1>Huston, we have a problem...</h1>'.$scrollTo;
+
+?>
