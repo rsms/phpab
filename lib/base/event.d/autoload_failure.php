9fffadb140a8bd81f7fa8ddad1a7ec4b45aea75b
rasmus <rasmus@9b4f6404-e40b-0410-ad9e-9008e25ae5f1>
2006-07-02 00:24:05 +0000

    Switched trigger_error with die, avoiding a possible lockout w __errhandler
    git-svn-id: http://svn.hunch.se/ab/trunk@95 9b4f6404-e40b-0410-ad9e-9008e25ae5f1

--- lib/base/event.d/autoload_failure.php
+++ lib/base/event.d/autoload_failure.php
@@ -7,11 +7,11 @@
 
 $t = debug_backtrace();
 array_shift($t);
-$msg = 'Failed to load class '.$c . ".<br/>\n"
+$msg = '<b>Failed to load class '.$c . "</b><br/>\n"
 	.'Include path: "' . ini_get('include_path') . "\"<br/>";
 
 if(ini_get('html_errors') == '0')
 	$msg = strip_tags($msg);
 
-trigger_error($msg, E_USER_ERROR);
+die($msg);
 ?>
