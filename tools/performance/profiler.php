ed6806de6d79a04a22449384d01b2807888f2ca9
rasmus <rasmus@9b4f6404-e40b-0410-ad9e-9008e25ae5f1>
2006-11-14 18:55:11 +0000

    git-svn-id: http://svn.hunch.se/ab/trunk@119 9b4f6404-e40b-0410-ad9e-9008e25ae5f1

--- tools/performance/profiler.php
+++ tools/performance/profiler.php
@@ -2,6 +2,11 @@
 apd_set_pprof_trace();
 require_once '../../lib/base/boot.php';
 
+$f = new File(__FILE__);
+var_dump($f->isDir());
+var_dump($f->isFile());
+$f->copyTo('/tmp/test');
+
 #$t = getrusage();
 #$t = doubleval($t['ru_utime.tv_sec']*1000) + doubleval($t['ru_utime.tv_usec']/1000000);
 #printf("User time: %f", $t);
